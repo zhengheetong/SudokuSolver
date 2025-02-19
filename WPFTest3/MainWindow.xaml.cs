eb7529886e2773146d9de58aa4a98711f6f82c20
TZH535 <zhenghee535@gmail.com>
2025-02-19 16:17:25 +0800

    Small update

--- WPFTest3/MainWindow.xaml.cs
+++ WPFTest3/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,6 +8,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -17,6 +19,11 @@ namespace WPFTest3;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private static readonly Regex _regex = new("[^1-9.-]+");
+    private static bool IsTextAllowed(string text) => !_regex.IsMatch(text);
+    private void Integer(object sender, TextCompositionEventArgs e) => e.Handled = !IsTextAllowed(e.Text);
+
+
     public TextBox[,] Position = new TextBox[9, 9];
 
     public MainWindow()
@@ -24,15 +31,39 @@ public partial class MainWindow : Window
         InitializeComponent();
         for (int i = 1; i <= 9; i++)
         {
+            Brush brush1 = Brushes.LightGreen;
+            Brush brush2 = Brushes.White;
+            if (i > 3 && i < 7)
+            {
+                brush1 = Brushes.White;
+                brush2 = Brushes.LightGreen;
+            }
             for (int j = 1; j <= 9; j++)
             {
-                Position[i-1,j-1] = (TextBox)FindName("Cell" + i + j);
+                Position[i - 1, j - 1] = (TextBox)FindName("Cell" + i + j);
+                Position[i - 1, j - 1].FontSize = 30;
+                Position[i - 1, j - 1].HorizontalContentAlignment = HorizontalAlignment.Center;
+                Position[i - 1, j - 1].VerticalContentAlignment = VerticalAlignment.Center;
+                Position[i - 1, j - 1].MaxLength = 1;
+                Position[i - 1, j - 1].PreviewTextInput += Integer; //only allow 1~9
+                Position[i - 1, j - 1].Background = brush1;
+                if (j > 3 && j < 7) Position[i - 1, j - 1].Background = brush2;
+                Grid.SetRow(Position[i - 1, j - 1], i-1);
+                Grid.SetColumn(Position[i - 1, j - 1], j-1);
             }
         }
     }
 
     private void ButtonSolve_Click(object sender, RoutedEventArgs e)
     {
-        Position[0, 1].Text = Position[0, 0].Text;
+        foreach(TextBox cell in Position)
+        {
+            Trace.WriteLine(cell.Name.ToString());
+        }
+    }
+
+    private void SudokuSolving()
+    {
+
     }
 }
