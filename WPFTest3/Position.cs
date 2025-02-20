b8362118e19c544e103096450d5bd15a90353528
TZH535 <zhenghee535@gmail.com>
2025-02-20 14:27:35 +0800

    Created new Class

---
+++ WPFTest3/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPFTest3
+{
+    class Position
+    {
+        public TextBox TextBox { get; set; } = new TextBox();
+        public int row { get; set; }
+        public int column { get; set; }
+    }
+}
